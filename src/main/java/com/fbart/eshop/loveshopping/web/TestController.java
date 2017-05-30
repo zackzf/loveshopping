465a9745ef1842d1a55372b95248411c92501cfe
zfwang <zhengfa.wang@foxmail.com>
2017-05-30 08:57:14 +0800

    first commit

---
+++ src/main/java/com/fbart/eshop/loveshopping/web/TestController.java
@@ -0,0 +1,12 @@
+package com.fbart.eshop.loveshopping.web;
+
+public class TestController {
+	public void testLogin(){
+		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+	public static void main(String[] args) {
+		String t1="";
+	}
+}
